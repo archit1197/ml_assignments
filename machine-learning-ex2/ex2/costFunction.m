f959ef423cbd1cd5c1be5fd55d1608baafd9f818
archit1197 <archit1197@gmail.com>
2017-01-20 23:30:09 +0530

    logistic regression

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -24,10 +24,11 @@ for i = 1:m
 	J=J-y(i)*log(sigmoid(theta'*X(i,:)'))-(1-y(i))*log(1-sigmoid(theta'*X(i,:)'));
 
 end
-
+summ=0;
 for j=1:t
+	summ=0;
 	for i=1:m
-		summ = (log(sigmoid(theta'*X(i,:)'))-y(i))*X(i,j);
+		summ = summ+(sigmoid(theta'*X(i,:)')-y(i))*X(i,j);
 	end
 	grad(j)=summ;
 end
